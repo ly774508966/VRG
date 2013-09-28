5dbc177863a3479081a48926375e5dbb3d8a4d1c
schwin42 <schwin42@gmail.com>
2013-09-27 23:44:56 -0700

    3D Menu Prototype

---
+++ Assets/Script3DMenu.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class Script3DMenu : MonoBehaviour {
+	/*
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 //OnSelected(true);
+	//	yield return new WaitForSeconds(1);
+	//	OnSelected(false);
+	}
+	
+	void OnSelected(bool on){
+		if(on){
+			iTween.MoveTo(gameObject,Vector3(0,0,-1),0.5);
+		} else {
+			{
+				iTween.MoveTo(gameObject,Vector3(0,0,0),0.5);
+			}
+	}
+	*/
+}
+	
