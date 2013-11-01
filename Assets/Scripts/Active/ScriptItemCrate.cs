05c4d654d203c2bb21161cfdd74c31d88e6e1f19
schwin42 <schwin42@gmail.com>
2013-11-01 13:31:59 -0700

    Configuration of NGUI

--- Assets/Scripts/Active/ScriptItemCrate.cs
+++ Assets/Scripts/Active/ScriptItemCrate.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class ScriptItemCrate : MonoBehaviour {
 	
 	public GameObject lid;
+	public GameObject xSide0;
+	public GameObject xSide1;
+	public GameObject ySide;
 	public Light crateLight;
 	public Color crateColor;
 	public Material crateMaterial;
@@ -14,6 +17,10 @@ public class ScriptItemCrate : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		lid = transform.FindChild("ObjCrateLid").gameObject;
+		xSide0 = transform.FindChild("ObjCrateXSide0").gameObject;
+		xSide1 = transform.FindChild("ObjCrateXSide1").gameObject;
+		ySide = transform.FindChild("ObjCrateYSide").gameObject;
+		
 		crateLight = transform.FindChild("LigCratePoint").gameObject.GetComponent<Light>();
 		audioSource = GetComponent<AudioSource>();
 		
@@ -43,7 +50,10 @@ public class ScriptItemCrate : MonoBehaviour {
 		lid.rigidbody.isKinematic = false;
 		lid.rigidbody.WakeUp();
 		lid.rigidbody.AddForce((Random.value * 2 - 1) * 100, Y_FORCE, Random.value * -500);
-		crateMaterial.SetColor("_Color", Color.white);
+		lid.renderer.material.color = Color.white;
+		xSide0.renderer.material.color = Color.white;
+		xSide1.renderer.material.color = Color.white;
+		ySide.renderer.material.color = Color.white;
 		crateLight.gameObject.SetActive(false);
 		audioSource.Play ();
 	}
@@ -61,8 +71,12 @@ public class ScriptItemCrate : MonoBehaviour {
 		//{
 		//child.gameObject.renderer.material.color = crateColor;		
 		//}
+		lid.renderer.material.color = crateColor;
+		xSide0.renderer.material.color = crateColor;
+		xSide1.renderer.material.color = crateColor;
+		ySide.renderer.material.color = crateColor;
 		
-		crateMaterial.SetColor("_Color", crateColor);
+		//crateMaterial.SetColor("_Color", crateColor);
 		crateLight.color = crateColor;
 		
 	}
